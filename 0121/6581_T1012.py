3ba77d59376a9039027e04f95a95f33268446762
hunmin-hub <hakddal707@naver.com>
2021-01-21 23:46:10 +0900

    김기민_T1012
    주석처리

--- 0121/6581_T1012.py
+++ 0121/6581_T1012.py
@@ -1,27 +1,21 @@
 import sys
 full_sentence=""
-for sentence in sys.stdin:
+for sentence in sys.stdin: # ctrl+D 입력까지 모든 줄을 입력 받는다
     full_sentence+=sentence
 
-words=full_sentence.split()
-result_sentence=[]
-for word in words :
-    if word=='<br>' :
-        result_sentence.append('\n')
-    else :
-        result_sentence.append(word)
+result_sentence=full_sentence.split() # 다중 공백 제거 및 단어 별 리스트화
 
 hr=""
 temp_list=[]
 temp_line_text=""
-for word in result_sentence :
-    if word=='\n' :
+for word in result_sentence : # 단어별 탐색
+    if word=='<br>' : # <br> 태그면 지금까지 출력해야할 문장 출력 후 '줄바꿈'
         temp_line_text = ' '.join(temp_list)
         temp_list = []
         print(temp_line_text)
-    elif word=='<hr>' :
+    elif word=='<hr>' : # 구분선 출력
         hr=""
-        if len(temp_list)!=0 :
+        if len(temp_list)!=0 : # 첫지점에서 구분선 출력이 아니라면, 지금까지 출력해야할 문장 출력 후 구분선 출력
             temp_line_text=' '.join(temp_list)
             temp_list=[]
             print(temp_line_text)
@@ -29,14 +23,14 @@ for word in result_sentence :
             hr+='-'
         print(hr)
     else :
-        temp_list.append(word)
+        temp_list.append(word) # 단어들이면 계속 합쳐준다
         temp_line_text=' '.join(temp_list)
-        if len(temp_line_text)>80 :
-            temp_list.pop(-1)
-            temp_line_text = ' '.join(temp_list)
+        if len(temp_line_text)>80 : # 합치다가 80줄이 넘으면
+            temp_list.pop(-1) # 넘는 단어 빼고
+            temp_line_text = ' '.join(temp_list) # 다시 합쳐서 출력
             print(temp_line_text)
             temp_list=[]
-            temp_list.append(word)
-temp_line_text=' '.join(temp_list)
+            temp_list.append(word) # 다음 줄은 넘는 단어부터 시작
+temp_line_text=' '.join(temp_list) # 마지막 줄 출력
 print(temp_line_text)
 
